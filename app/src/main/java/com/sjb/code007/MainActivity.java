b8b3d36d12d0d7cdbb8f2c23b421046d8970e01b
Suraj Bind <surajbindpro@gmail.com>
2021-07-28 11:34:06 +0530

    First commit

---
+++ app/src/main/java/com/sjb/code007/MainActivity.java
@@ -0,0 +1,28 @@
+package com.sjb.code007;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
+
+    }
+}
